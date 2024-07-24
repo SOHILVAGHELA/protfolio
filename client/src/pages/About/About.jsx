3b77c6b7adeea5cd0b60d207ed2cea62f28f9aa7
SOHILVAGHELA <sohilvaghela7863@gmail.com>
2024-07-24 17:43:50 +0530

    improve info

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -12,16 +12,12 @@ function About() {
           <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
             <h1>About me</h1>
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
-              reprehenderit consequatur quasi eius ullam! Nesciunt totam laborum
-              debitis quia maiores minima tempore odio quam. Neque repellat
-              dolore deserun blanditiis cuue! Ad obcaecati ea quidem voluptas
-              nostrum! Architecto, repellat cupiditate. Id inventore aliquid
-              labore, illum velit saepe aut, placeat earum quod quae enim
-              deleniti quos ipsa exercitationem ex harum nesciunt quas
-              architecto qui consequuntur expedita! Dolor iure provident beatae
-              nostrum. Quas laboriosam dolore nesciunt porro nisi maxime
-              asperiores distinctio consectetur.
+              I am a passionate and dedicated Full Stack Developer with a
+              Bachelor of Computer Applications (BCA) degree from KSV
+              University. I have successfully completed a comprehensive full
+              stack development course from Internshala, where I built an
+              e-commerce project and a Twitter clone using React, JavaScript,
+              Node.js, MongoDB, and Express.js.
             </p>
           </div>
         </div>
